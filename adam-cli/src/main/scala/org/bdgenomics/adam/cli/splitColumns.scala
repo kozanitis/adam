48e3aa9b5daf540ae6145c35c0b0320aebdef5c3
Kozanitis <kozanitis@berkeley.edu>
2015-03-24 05:21:15 -0700

    Enhance SplitColumns with pileUp info

--- adam-cli/src/main/scala/org/bdgenomics/adam/cli/splitColumns.scala
+++ adam-cli/src/main/scala/org/bdgenomics/adam/cli/splitColumns.scala
@@ -20,15 +20,9 @@ import org.kohsuke.args4j.Argument
 import org.apache.spark.SparkContext
 import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.rdd.RDD
-import org.apache.spark.rdd.MetricsContext._
 import org.bdgenomics.adam.rdd.ADAMContext._
-import org.bdgenomics.formats.avro.AlignmentRecord
-import parquet.hadoop.metadata.CompressionCodecName
-import org.bdgenomics.adam.util.{ ParquetLogger, HadoopUtil }
-import parquet.hadoop.ParquetOutputFormat
-import parquet.avro.AvroParquetOutputFormat
-import parquet.hadoop.util.ContextUtil
-import org.apache.avro.reflect.ReflectData
+import org.bdgenomics.formats.avro.{ Pileup, AlignmentRecord }
+import org.bdgenomics.adam.core._
 
 object SplitColumns extends ADAMCommandCompanion {
   val commandName = "splitcolumns"
@@ -39,60 +33,49 @@ object SplitColumns extends ADAMCommandCompanion {
   }
 }
 
-class SplitColumnsArgs extends Args4jBase with ParquetArgs {
+class SplitColumnsArgs extends Args4jBase with ParquetArgs with Serializable {
   @Argument(required = true, metaVar = "ADAM",
     usage = "The ADAM Variant file", index = 0)
   var adamFile: String = _
 
-  @Argument(required = true, metaVar = "OutputFile",
-    usage = "The output file", index = 1)
-  var outputFile: String = null
 }
 
 class SplitColumns(val args: SplitColumnsArgs) extends ADAMSparkCommand[SplitColumnsArgs] with Serializable {
   val companion = SplitColumns
 
   def run(sc: SparkContext, job: Job) {
-    val reads: RDD[AlignmentRecord] = sc.loadAlignments(args.adamFile)
-    val smallObject = reads.map(x => SubsetOfAdam(x.getReadName, x.getContig.getContigName,
-      x.getStart, x.getEnd, x.getCigar, x.getMapq, x.getFirstOfPair))
-    parquetSave(smallObject, args.outputFile)
+    val CoordinateSubsetFile = args.adamFile + ".sub"
+    val PileupSubsetFile = args.adamFile + ".psub"
+    val reads: RDD[AlignmentRecord] = sc.loadAlignments(args.adamFile).filter(_.getReadMapped)
+    val pileups = reads.adamRecords2Pileup()
+    val smallObject = reads.map(getCoordinateSubset(_))
+    smallObject.adamParquetSave(CoordinateSubsetFile)
+    val smallPileups = pileups.map(getPileupSubset(_))
+    smallPileups.adamParquetSave(PileupSubsetFile)
   }
 
-  def parquetSave[T: Manifest](rdd: RDD[T],
-                               filePath: String,
-                               blockSize: Int = 128 * 1024 * 1024,
-                               pageSize: Int = 1 * 1024 * 1024,
-                               compressCodec: CompressionCodecName = CompressionCodecName.GZIP,
-                               disableDictionaryEncoding: Boolean = false) {
-    //log.info("Saving data in ADAM format")
+  def getCoordinateSubset(x: AlignmentRecord) = {
+    val builder = CoordinateSubset.newBuilder()
+    builder.setReadName(x.getReadName)
+    builder.setChromosome(x.getContig.getContigName)
+    builder.setStart(x.getStart)
+    builder.setEnd(x.getEnd)
+    builder.setCigar(x.getCigar)
+    builder.setMapq(x.getMapq)
+    builder.setFirstOfPair(x.getFirstOfPair)
+    builder.build()
+  }
 
-    /*if (!filePath.endsWith(".adam")) {
-      log.error("Filename does not end with *.adam extension. File will be saved, but must be renamed to be opened.")
-    }*/
-
-    val job = HadoopUtil.newJob(rdd.context)
-    //ParquetLogger.hadoopLoggerLevel(Level.SEVERE)
-    ParquetOutputFormat.setCompression(job, compressCodec)
-    ParquetOutputFormat.setEnableDictionary(job, !disableDictionaryEncoding)
-    ParquetOutputFormat.setBlockSize(job, blockSize)
-    ParquetOutputFormat.setPageSize(job, pageSize)
-    //AvroParquetOutputFormat.setSchema(job, manifest[T].runtimeClass.asInstanceOf[Class[T]].newInstance().getSchema)
-    val schema = ReflectData.get().getSchema(manifest[T].getClass())
-    AvroParquetOutputFormat.setSchema(job, schema)
-    // Add the Void Key
-    val recordToSave = rdd.map(p => (null, p))
-    // Save the values to the ADAM/Parquet file
-    recordToSave.saveAsNewAPIHadoopFile(filePath,
-      classOf[java.lang.Void], manifest[T].runtimeClass.asInstanceOf[Class[T]], classOf[AvroParquetOutputFormat],
-      ContextUtil.getConfiguration(job))
+  def getPileupSubset(x: Pileup) = {
+    val builder = PileupSubset.newBuilder()
+    if (x.getContig.getContigName != null)
+      builder.setChromosome(x.getContig.getContigName)
+    builder.setPosition(x.getPosition)
+    builder.setMapq(x.getMapQuality)
+    if (x.getReadBase != null)
+      builder.setReadBase(x.getReadBase.toString)
+    builder.setIsReverseStrand(x.getIsReverseStrand)
+    builder.build()
   }
 }
 
-case class SubsetOfAdam(readName: String,
-                        chromosome: String,
-                        start: Long,
-                        end: Long,
-                        cigar: String,
-                        mapq: Int,
-                        firstOfPair: Boolean) extends Serializable
